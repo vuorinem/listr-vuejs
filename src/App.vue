c23e98be42d7ab5ae6bdf15e298431f5a1a3b689
Mikko Vuorinen <vuorinem@gmail.com>
2020-02-28 16:44:39 +0000

    Add Item component

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,16 @@
 <template>
   <div id="app">
-    TODO
+    <Item label="First Item" />
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator'
+import Item from './list/Item.vue'
 
 @Component({
   components: {
+    Item
   }
 })
 export default class App extends Vue {}
---
+++ src/list/Item.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="listr-item">
+    <span class="listr-item-label">{{ label }}</span>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop } from 'vue-property-decorator'
+
+// Define the component in class-style
+@Component
+export default class Item extends Vue {
+  @Prop({ default: '' })
+  label!: string;
+}
+</script>
+
+<style lang="scss">
+</style>
