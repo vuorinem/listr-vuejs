cf23f29ede04ca2f8ecd031e8fb981834f867a1d
Mikko Vuorinen <vuorinem@gmail.com>
2020-04-04 21:33:48 +0100

    Add API calls and socket.io to the list-api

--- src/list/list-api.ts
+++ src/list/list-api.ts
@@ -1,42 +1,55 @@
+import { connect } from 'socket.io-client';
+
+const currentHost = window.location.hostname;
+const socketClient = connect(`http://${currentHost}:3004`);
+
 export type ItemData = {
-  label: string;
-  link?: string;
-  count: number;
-  reservedCount: number;
-  myReservedCount: number;
-};
+    label: string;
+    link?: string;
+    count: number;
+    reservedCount: number;
+    myReservedCount: number;
+}
 
 export type ListData = {
-  name: string;
-  items: ItemData[];
-};
+    name: string;
+    items: ItemData[];
+}
 
-export function getList (name: string): Promise<ListData> {
-  // todo: Make the API call
-  return Promise.resolve({
-    name: name,
-    items: [
-      {
-        label: 'First item',
-        count: 1,
-        link: 'https://www.google.com',
-        reservedCount: 0,
-        myReservedCount: 0
-      },
-      {
-        label: 'Second item',
-        count: 1,
-        link: 'https://www.google.com',
-        reservedCount: 1,
-        myReservedCount: 0
-      },
-      {
-        label: 'Third item',
-        count: 1,
-        link: 'https://www.google.com',
-        reservedCount: 1,
-        myReservedCount: 1
-      }
-    ]
-  } as ListData)
+export type Unsubscribe = () => void;
+
+export async function getList(name: string): Promise<ListData | null> {
+    const response = await fetch(`http://${currentHost}:3003/list?name=${name}`);
+
+    if (!response.ok) {
+        return null;
+    }
+
+    return response.json();
+}
+
+export async function reserve(listName: string, itemLabel: string): Promise<boolean> {
+    const response = await fetch(`http://${currentHost}:3003/list/reserve?name=${listName}&item=${itemLabel}`, {
+        method: 'post',
+    });
+
+    return response.ok;
+}
+
+export async function cancel(listName: string, itemLabel: string): Promise<boolean> {
+    const response = await fetch(`http://${currentHost}:3003/list/cancel?name=${listName}&item=${itemLabel}`, {
+        method: 'post',
+    });
+
+    return response.ok;
+}
+
+export function subscribe(listName: string, onChange: () => void): Unsubscribe {
+    const subscription = socketClient.on(listName, () => {
+        onChange();
+    });
+
+    return () => {
+        subscription.off(listName);
+    };
 }
